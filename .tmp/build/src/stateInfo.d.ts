97962178288a4559501f519f72bef8d90c86822b
grxzzzone <xutu86@gmail.com>
2021-08-19 10:09:18 -0400

    Delete .tmp directory

--- .tmp/build/src/stateInfo.d.ts
+++
@@ -1,7 +0,0 @@
-export interface stateInfoType {
-    "ISO Alpha-3 Code": string;
-    "Latitude": number;
-    "Longitude": number;
-    "UN State Name": string;
-}
-export declare let stateInfo: stateInfoType[];
